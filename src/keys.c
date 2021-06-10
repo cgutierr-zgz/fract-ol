24baac7606214c7d27233b29728c17af7915d63d
Carlos <carlosgutimo.3d@gmail.com>
2021-06-10 23:42:27 +0200

    10/06/2021 23:42

--- src/keys.c
+++ src/keys.c
@@ -6,7 +6,7 @@
 /*   By: cgutierr <cgutierr@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/10 18:45:44 by cgutierr          #+#    #+#             */
-/*   Updated: 2021/06/10 23:18:02 by cgutierr         ###   ########.fr       */
+/*   Updated: 2021/06/10 23:30:28 by cgutierr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,7 @@
 
 int destroy(t_fractol *fractol)
 {
-	int x = fractol->julia;
-	x++;
+	fractol->mdlbr.selected = 0; // same with julia
 	exit(0);
 }
 
@@ -70,37 +69,11 @@ int keys_release(int keycode, t_fractol *fractol)
 
 int mouse_hook(int keycode, int x, int y, t_fractol *fractol)
 {
-	if(x > fractol->screen.x / 2)
-	{
-	//	fractol->mdlbr.move_left = 0;
-
-	//	fractol->mdlbr.move_right = 1;// estamos pa la derecha
-	fractol->mdlbr.moveX += 0.1* (x / (x / fractol->screen.x));
-	}
-	else
-	{
-	fractol->mdlbr.moveX -= 0.1* (x / (x / fractol->screen.x));
-	//	fractol->mdlbr.move_left = 1; // tamos pa la izq
-
-	//	fractol->mdlbr.move_right = 0;// estamos pa la derecha
-	}
-
-	if(y > fractol->screen.y / 2)
-	{
-	fractol->mdlbr.moveY -= 0.1* (y / (y / fractol->screen.y));
-	//	fractol->mdlbr.move_down = 1; // estamos pabajo
-	//fractol->mdlbr.move_up = 0;
-
-	}
-	else
-	{
-	fractol->mdlbr.moveY += 0.1 * (y / (y / fractol->screen.y));
-//fractol->mdlbr.move_down = 0;
-//	fractol->mdlbr.move_up = 1;  // tamos parriba
-	}
+	// TODO: Zoom to mouse position
+	x++;
+	y++;
 	if (fractol->mdlbr.selected)
 	{
-
 		if (keycode == SCROLL_UP)
 			fractol->mdlbr.zoom_out = 1;
 		if (keycode == SCROLL_DOWN && fractol->mdlbr.zoom > 0.1)
