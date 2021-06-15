b5dae58433bb8d41756cf09257211bf57a60048a
Carlos <carlosgutimo.3d@gmail.com>
2021-06-15 21:40:16 +0200

    15/06/2021 21:40

--- src/trees.c
+++ src/trees.c
@@ -6,52 +6,17 @@
 /*   By: cgutierr <cgutierr@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/10 11:53:23 by cgutierr          #+#    #+#             */
-/*   Updated: 2021/06/15 20:11:37 by cgutierr         ###   ########.fr       */
+/*   Updated: 2021/06/15 21:36:20 by cgutierr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fractol.h"
-/*
-void recursion(double posX, double posY, double dirX, double dirY, double size, int n,
-			   t_fractol *fractol)
+
+int	trees(t_fractol *fractol)
 {
-	double pi = 3.1415926535897932384626433832795;
+	int	x;
 
-	double angle = 0.2 * pi; //angle in radians
-	double shrink = 1.8;	 //relative size of new branches
-
-	int x1, x2, y1, y2;
-	int x3, x4, y3, y4;
-	x1 = (int)(posX);
-	y1 = (int)(posY);
-	x2 = (int)(posX + size * dirX);
-	y2 = (int)(posY + size * dirY);
-	//	if (clipLine(x1, y1, x2, y2, x3, y3, x4, y4))
-	//		drawLine(x3, y3, x4, y4, ColorRGB(128, 96, 64));
-	//	fractol->color = 23423;
-	//	my_pixel_put(&fractol->main_img, x1, y1, fractol->color);
-
-	if (n >= fractol->maxIterations)
-		return;
-
-	double posX2, posY2, dirX2, dirY2, size2;
-	int n2;
-	posX2 = posX + size * dirX;
-	posY2 = posY + size * dirY;
-	size2 = size / shrink;
-	n2 = n + 1;
-	dirX2 = cos(angle) * dirX + sin(angle) * dirY;
-	dirY2 = -sin(angle) * dirX + cos(angle) * dirY;
-	recursion(posX2, posY2, dirX2, dirY2, size2, n2, fractol);
-	dirX2 = cos(-angle) * dirX + sin(-angle) * dirY;
-	dirY2 = -sin(-angle) * dirX + cos(-angle) * dirY;
-	recursion(posX2, posY2, dirX2, dirY2, size2, n2, fractol);
-}
-*/
-int trees(t_fractol *fractol)
-{
-//	recursion(fractol->screen.x / 2, fractol->screen.y - 1, 0, -1, fractol->screen.y / 2.3, 0, fractol);
-int x = fractol->moveY;
-x++;
+	x = fractol->moveY;
+	x++;
 	return (0);
 }
