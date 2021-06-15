5126cb99b7a8276d491fb90ee98c7e1a6eedcd8d
Carlos <carlosgutimo.3d@gmail.com>
2021-06-15 23:49:53 +0200

    15/06/2021 23:49

--- src/sierpinski.c
+++ src/sierpinski.c
@@ -6,26 +6,29 @@
 /*   By: cgutierr <cgutierr@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/10 11:53:23 by cgutierr          #+#    #+#             */
-/*   Updated: 2021/06/15 21:19:15 by cgutierr         ###   ########.fr       */
+/*   Updated: 2021/06/15 23:49:39 by cgutierr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fractol.h"
 
-static void	mainsierpinki(int *x, int y, int *i, t_fractol *fractol)
+static void	mainsierpinki(int *x, int y, t_fractol *fractol)
 {
+	//int random = rand();
+	//int random2 = rand();
+	//int random3 = rand();
 	if (!((*x / 1) % 3 == 1 && (y / 1) % 3 == 1) && !((*x / 3) % 3 == 1
 			&& (y / 3) % 3 == 1) && !((*x / 9) % 3 == 1
 			&& (y / 9) % 3 == 1) && !((*x / 27) % 3 == 1
 			&& (y / 27) % 3 == 1) && !((*x / 81) % 3 == 1
 			&& (y / 81) % 3 == 1))
 	{
-		set_color(fractol, i);
+		fractol->color = create_trgb(0, cos(y) * 4, *x + y * 2, y + *x * 2);
 		my_pixel_put(&fractol->main_img, *x, y, fractol->color);
 	}
 	else
 	{
-		fractol->color = create_trgb(0, 0, 0, 0);
+		fractol->color = create_trgb(250, 0, 0, 0);// create_trgb(0, 0, 0, 0);
 		my_pixel_put(&fractol->main_img, *x, y, fractol->color);
 	}
 	*x += 1;
@@ -40,10 +43,10 @@ int	sierpinski(t_fractol *fractol)
 	y = 0;
 	while (y < fractol->screen.y)
 	{
-		i = rand() * sin(x * y) / 2;
+		i = sin(x * y) / 2;
 		x = 0;
 		while (x < fractol->screen.x)
-			mainsierpinki(&x, y, &i, fractol);
+			mainsierpinki(&x, y, fractol);
 		y++;
 	}
 	return (0);
--- src/trees.c
+++ src/trees.c
@@ -6,17 +6,30 @@
 /*   By: cgutierr <cgutierr@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/10 11:53:23 by cgutierr          #+#    #+#             */
-/*   Updated: 2021/06/15 21:36:20 by cgutierr         ###   ########.fr       */
+/*   Updated: 2021/06/15 23:17:47 by cgutierr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fractol.h"
 
-int	trees(t_fractol *fractol)
+static void draw_line(int x0, int y0, int x1, int y1, t_fractol *fractol)
 {
-	int	x;
+	while (x0 < x1)
+	{
+		my_pixel_put(&fractol->main_img, x0, y0, create_trgb(0, 0, 0, 0));
+		while (y0 < y1)
+		{
+			my_pixel_put(&fractol->main_img, x0, y0, 31243);
+			y0++;
+			break;
+		}
 
-	x = fractol->moveY;
-	x++;
+		x0++;
+	}
+}
+
+int trees(t_fractol *fractol)
+{
+	draw_line(0, 0, fractol->screen.x, fractol->screen.y, fractol);
 	return (0);
 }
