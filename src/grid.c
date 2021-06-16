55bb4a6881e5058a59b7e9d0c03523baa788afea
Carlos <carlosgutimo.3d@gmail.com>
2021-06-16 12:43:32 +0200

    16/06/2021 12:43

--- src/grid.c
+++ src/grid.c
@@ -6,16 +6,16 @@
 /*   By: cgutierr <cgutierr@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/10 11:53:23 by cgutierr          #+#    #+#             */
-/*   Updated: 2021/06/16 12:38:42 by cgutierr         ###   ########.fr       */
+/*   Updated: 2021/06/16 12:43:27 by cgutierr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fractol.h"
 
-static void draw_line(int x0, int y0, t_fractol *fractol)
+static void	draw_line(int x0, int y0, t_fractol *fractol)
 {
-	int x1;
-	int y1;
+	int	x1;
+	int	y1;
 
 	x1 = fractol->screen.x;
 	y1 = fractol->screen.y;
@@ -26,16 +26,16 @@ static void draw_line(int x0, int y0, t_fractol *fractol)
 		{
 			my_pixel_put(&fractol->main_img, x0, y0, fractol->color);
 			y0++;
-			break;
+			break ;
 		}
 		x0++;
 	}
 }
 
-int grid(t_fractol *fractol)
+int	grid(t_fractol *fractol)
 {
-	int x;
-	int y;
+	int	x;
+	int	y;
 
 	x = 0;
 	fractol->color = create_trgb(0, 0, rand(), 0);
@@ -45,7 +45,8 @@ int grid(t_fractol *fractol)
 		while (y < fractol->screen.y)
 		{
 			my_pixel_put(&fractol->main_img, x, y, create_trgb(0, 0, 0, 0));
-			if (x % fractol->maxIterations == 0 || y % fractol->maxIterations == 0)
+			if (x % fractol->maxIterations == 0
+				|| y % fractol->maxIterations == 0)
 				draw_line(x, y, fractol);
 			y++;
 		}
